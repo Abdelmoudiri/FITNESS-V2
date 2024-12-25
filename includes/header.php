affce27224f98a2a18a47678d75bd132afda86ce
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-12-25 12:27:13 +0100

    l'ajout de header et footer

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<header class="bg-gradient-to-r from-purple-900 via-purple-500 to-purple-900 text-white shadow-2xl">
+        <nav class="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+            <a href="#" class="text-2xl font-bold">PowerMove</a>
+            <ul class="hidden md:flex space-x-6 text-lg">
+                <li><a href="index.php" class="hover:text-blue-300">Accueil</a></li>
+                <li><a href="#all-activities" class="hover:text-blue-300">Activités</a></li>
+                <li><a href="reservations.php" class="hover:text-blue-300">Réservations</a></li>
+                <li><a href="liste_membres.php" class="hover:text-blue-300">Membres</a></li>
+            </ul>
+            <button id="menuBtn" class="md:hidden text-2xl focus:outline-none">☰</button>
+        </nav>
+        <div id="mobileMenu" class="hidden bg-gradient-to-r from-purple-900 via-purple-500 to-purple-900 text-white shadow-2xl text-white slide-down md:hidden">
+            <ul class="space-y-4 text-center p-4">
+                <li><a href="index.php" class="block hover:bg-purple-900">Accueil</a></li>
+                <li><a href="#all-activities" class="block hover:bg-purple-900">Activités</a></li>
+                <li><a href="reservations.php" class="block hover:bg-purple-900">Réservations</a></li>
+                <li><a href="liste_membres.php" class="block hover:bg-purple-900">Membres</a></li>
+            </ul>
+        </div>
+    </header>
