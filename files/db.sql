ce3bf3b104c0440b83e8e5d6dd1a7114e9551ba1
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-12-25 18:12:01 +0100

    l'ajout du script des requetes SQL

--- files/db.sql
+++ files/db.sql
@@ -1,64 +1,64 @@
-CREATE DATABASE gymsport;
-USE gymsport;
+CREATE DATABASE fitness_V2;
+USE fitness_V2;
 
-CREATE TABLE `activites` (
-  `ID_Activite` int(11) AUTO_INCREMENT PRIMARY key NOT NULL,
-  `Nom_Activite` varchar(100) NOT NULL,
-  `Description` text DEFAULT NULL,
-  `Capacite` int(11) NOT NULL,
+CREATE TABLE `user` (
+  `id_user` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL UNIQUE,
+  `telephone` varchar(15) DEFAULT NULL,
+  `password` varchar(20) NOT NULL,
+  `role` ENUM('admin', 'member') DEFAULT 'member'
+);  
+
+CREATE TABLE `activities` (
+  `id_activity` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `nom_activite` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `capacite` int(11) NOT NULL,
   `date_debut` date NOT NULL,
   `date_fin` date NOT NULL,
-  `Disponibilite` tinyint(1) NOT NULL
+  `est_isponible` boolean DEFAULT true
 ) ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `membres`
---
-
-CREATE TABLE `membres` (
-  `ID_Membre` int(11) AUTO_INCREMENT NOT NULL,
-  `Nom` varchar(50) NOT NULL,
-  `Prenom` varchar(50) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Telephone` varchar(15) DEFAULT NULL,
-  `is_admin` tinyint(1) DEFAULT 0,
-  `password` varchar(255) NOT NULL
-);   
-
-
---
--- Déchargement des données de la table `membres`
---
-
 
 CREATE TABLE `reservations` (
   `ID_Reservation` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `ID_Membre` int(11) NOT NULL,
-  `ID_Activite` int(11) NOT NULL,
-  `Date_Reservation` datetime DEFAULT CURRENT_TIMESTAMP,
-  `Statut` enum('Confirmee','Annulee') NOT NULL DEFAULT 'Confirmee'
+  `id_user` int(11) NOT NULL,
+  `id_activity` int(11) NOT NULL,
+  `date_reservation` datetime DEFAULT CURRENT_TIMESTAMP,
+  `statut` enum('Confirmee','Annulee', 'En attente') NOT NULL DEFAULT 'En attente',
+  FOREIGN KEY (id_user) REFERENCES user(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (id_activity) REFERENCES activities(id_activity) ON DELETE CASCADE ON UPDATE CASCADE 
 );
-ALTER TABLE `reservations`
-ADD CONSTRAINT `fk_reservation_membre`
-FOREIGN KEY (`ID_Membre`) REFERENCES `membres`(`ID_Membre`)
-ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE `reservations`
-ADD CONSTRAINT `fk_reservation_activite`
-FOREIGN KEY (`ID_Activite`) REFERENCES `activites`(`ID_Activite`)
-ON DELETE CASCADE ON UPDATE CASCADE;
-
-INSERT INTO `reservations` (`ID_Membre`,`ID_Activite`)
-VALUES (1,1);
-INSERT INTO `activites` (`Nom_Activite`, `Description`, `Capacite`, `date_debut`, `date_fin`, `Disponibilite`)
+INSERT INTO `reservations` (`id_user`, `id_activity`)
 VALUES
-('Yoga Matinal', 'Une séance de yoga pour bien commencer la journée', 20, '2024-01-01', '2024-01-31', 1),
-('Cours de Zumba', 'Des sessions dynamiques pour brûler des calories en dansant', 30, '2024-02-01', '2024-02-28', 1),
-('Musculation Intense', 'Un programme pour développer la force musculaire', 15, '2024-03-01', '2024-03-31', 1),
-('Méditation Guidée', 'Se détendre et réduire le stress grâce à la méditation', 25, '2024-04-01', '2024-04-30', 1),
-('Natation Libre', 'Accès à la piscine pour nager librement', 10, '2024-05-01', '2024-05-31', 1),
-('Cours de Boxe', 'Apprenez les bases de la boxe avec un coach professionnel', 12, '2024-06-01', '2024-06-30', 1);
+(1, 5),
+(2, 4),
+(3, 3),
+(4, 2),
+(5, 1);
+
+
+
+
+
+INSERT INTO `activities` (`nom_Activite`, `description`, `capacite`, `date_debut`, `date_fin`)
+VALUES
+('Yoga Matinal', 'Une séance de yoga pour bien commencer la journée', 20, '2024-01-01', '2024-01-31'),
+('Cours de Zumba', 'Des sessions dynamiques pour brûler des calories en dansant', 30, '2024-02-01', '2024-02-28'),
+('Musculation Intense', 'Un programme pour développer la force musculaire', 15, '2024-03-01', '2024-03-31'),
+('Méditation Guidée', 'Se détendre et réduire le stress grâce à la méditation', 25, '2024-04-01', '2024-04-30'),
+('Natation Libre', 'Accès à la piscine pour nager librement', 10, '2024-05-01', '2024-05-31'),
+('Cours de Boxe', 'Apprenez les bases de la boxe avec un coach professionnel', 12, '2024-06-01', '2024-06-30');
+
+INSERT INTO `user` (`nom`, `prenom`, `email`, `telephone`, `password`)
+VALUES
+('Moudiri', 'Abdeljabbar', 'moudiri@gmail.com', '0666666666', 'moudiri'),
+('dadssi', 'mohamed', 'dadssi@gmail.com', '0666666666', 'dadssi'),
+('salma', 'salamat', 'salma@gmail.com', '0666666666', 'salma'),
+('chamkhi', 'mohamed', 'chamkhi@gmail.com', '0666666666', 'chamkhi'),
+('majdi', 'mehdi', 'majdi@gmail.com', '0666666666', 'majdi');
+
 
 
