3d5822fcd3c1e0c08c9001b10eb9d529a9a45563
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-12-26 11:36:55 +0100

    la finalisation de toutes les requetes SQL demandées

--- files/db.sql
+++ files/db.sql
@@ -9,7 +9,17 @@ CREATE TABLE `user` (
   `telephone` varchar(15) DEFAULT NULL,
   `password` varchar(20) NOT NULL,
   `role` ENUM('admin', 'member') DEFAULT 'member'
-);  
+);
+
+INSERT INTO `user` (`nom`, `prenom`, `email`, `telephone`, `password`)
+VALUES
+('Moudiri', 'Abdeljabbar', 'moudiri@gmail.com', '0666666666', 'moudiri'),
+('dadssi', 'mohamed', 'dadssi@gmail.com', '0666666666', 'dadssi'),
+('salma', 'salamat', 'salma@gmail.com', '0666666666', 'salma'),
+('chamkhi', 'mohamed', 'chamkhi@gmail.com', '0666666666', 'chamkhi'),
+('majdi', 'mehdi', 'majdi@gmail.com', '0666666666', 'majdi');
+
+
 
 CREATE TABLE `activities` (
   `id_activity` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -20,6 +30,15 @@ CREATE TABLE `activities` (
   `date_fin` date NOT NULL,
   `est_isponible` boolean DEFAULT true
 ) ;
+
+INSERT INTO `activities` (`nom_Activite`, `description`, `capacite`, `date_debut`, `date_fin`)
+VALUES
+('Yoga Matinal', 'Une séance de yoga pour bien commencer la journée', 20, '2024-01-01', '2024-01-31'),
+('Cours de Zumba', 'Des sessions dynamiques pour brûler des calories en dansant', 30, '2024-02-01', '2024-02-28'),
+('Musculation Intense', 'Un programme pour développer la force musculaire', 15, '2024-03-01', '2024-03-31'),
+('Méditation Guidée', 'Se détendre et réduire le stress grâce à la méditation', 25, '2024-04-01', '2024-04-30'),
+('Natation Libre', 'Accès à la piscine pour nager librement', 10, '2024-05-01', '2024-05-31'),
+('Cours de Boxe', 'Apprenez les bases de la boxe avec un coach professionnel', 12, '2024-06-01', '2024-06-30');
 
 CREATE TABLE `reservations` (
   `ID_Reservation` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -38,27 +57,28 @@ VALUES
 (3, 3),
 (4, 2),
 (5, 1);
-
-
-
-
-
-INSERT INTO `activities` (`nom_Activite`, `description`, `capacite`, `date_debut`, `date_fin`)
+INSERT INTO `reservations` (`id_user`, `id_activity`)
 VALUES
-('Yoga Matinal', 'Une séance de yoga pour bien commencer la journée', 20, '2024-01-01', '2024-01-31'),
-('Cours de Zumba', 'Des sessions dynamiques pour brûler des calories en dansant', 30, '2024-02-01', '2024-02-28'),
-('Musculation Intense', 'Un programme pour développer la force musculaire', 15, '2024-03-01', '2024-03-31'),
-('Méditation Guidée', 'Se détendre et réduire le stress grâce à la méditation', 25, '2024-04-01', '2024-04-30'),
-('Natation Libre', 'Accès à la piscine pour nager librement', 10, '2024-05-01', '2024-05-31'),
-('Cours de Boxe', 'Apprenez les bases de la boxe avec un coach professionnel', 12, '2024-06-01', '2024-06-30');
+(3, 5);
 
-INSERT INTO `user` (`nom`, `prenom`, `email`, `telephone`, `password`)
-VALUES
-('Moudiri', 'Abdeljabbar', 'moudiri@gmail.com', '0666666666', 'moudiri'),
-('dadssi', 'mohamed', 'dadssi@gmail.com', '0666666666', 'dadssi'),
-('salma', 'salamat', 'salma@gmail.com', '0666666666', 'salma'),
-('chamkhi', 'mohamed', 'chamkhi@gmail.com', '0666666666', 'chamkhi'),
-('majdi', 'mehdi', 'majdi@gmail.com', '0666666666', 'majdi');
+-- 1.	Combien de réservations ont été confirmées dans le système ?
+SELECT COUNT(*) FROM reservations WHERE statut = 'confirmee';
 
+-- 2.	Quelle est la capacité moyenne des activités proposées ?
 
+SELECT AVG(capacite) FROM activities;
 
+-- 3.	Combien de membres distincts ont effectué au moins une réservation ?
+
+SELECT COUNT(DISTINCT id_user) FROM reservations;
+
+-- 4.	Quelles sont les trois activités les plus réservées ?
+
+SELECT  DISTINCT a.nom_Activite as 'nom', COUNT(r.id_reservation) as 'totatR' FROM activities a JOIN reservations r on r.id_activity = a.id_activity
+            GROUP BY nom ORDER BY  COUNT('totatR') DESC LIMIT 3;
+
+-- 5.	Quel est le pourcentage des réservations annulées par rapport au total des réservations ?
+
+SELECT 
+    ((SELECT COUNT(*) FROM reservations WHERE statut = 'annulee') * 100.0 / COUNT(*)) AS pourcentage_annulations
+FROM reservations;
