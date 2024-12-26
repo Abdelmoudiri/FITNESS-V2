3fcb171c7e5cd52834fcfb64cc0f26838af5e775
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2024-12-26 13:23:36 +0100

    debut de la classe Member

--- models/Member.php
+++ models/Member.php
@@ -1,4 +1,15 @@
 <?php
+class Member extends User{
+    private $role;
+    public function __construct($nom,$prenom,$email,$telephone,$password)
+    {
+        parent::__construct($nom,$prenom,$email,$telephone,$password);
+        $this->role = 'member';
+    }
+    
+}
 
 
 ?>
+$m = new Member("a","a","e","a","a");
+
